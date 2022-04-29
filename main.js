8f53ea6cf7f369e32cd09a2a54e7c598404d184a
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-04-29 17:07:18 +0500

    dynamic input weather app express js

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-import express from 'express';
-import hbs from 'hbs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import requests from 'requests';
+import express from "express";
+import hbs from "hbs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import requests from "requests";
 const app = express();
 const port = process.env.port || 8000;
-const __dirname = dirname(fileURLToPath(
-    import.meta.url
-));
-const publicPath = path.join(__dirname, 'public');
-const viewPath = path.join(__dirname, 'screens/views');
-const partialsPath = path.join(__dirname, 'screens/partials');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const publicPath = path.join(__dirname, "public");
+const viewPath = path.join(__dirname, "screens/views");
+const partialsPath = path.join(__dirname, "screens/partials");
 console.log(`${publicPath} \n${viewPath} \n${partialsPath}`);
 console.log(__dirname);
 app.use(express.static(publicPath));
-app.set('view engine', 'hbs');
-app.set('views', viewPath);
+app.set("view engine", "hbs");
+app.set("views", viewPath);
 hbs.registerPartials(partialsPath);
-app.get('', (req, res) => {
-    res.render('index', { name: "Nabeel" });
+app.get("", (req, res) => {
+  res.render("index", { name: "Nabeel" });
 });
-app.get('/about', (req, res) => {
-    res.render('about', { name: "Nabeel" });
+app.get("/about", (req, res) => {
+  res.render("about", { name: "Nabeel" });
 });
-app.get('/weather', (req, res) => {
-    res.render('weather', { name: "Nabeel" });
+app.get("/weather", (req, res) => {
+  res.render("weather", { name: "Nabeel" });
 });
-app.get('*', (req, res) => {
-    res.render('error', { name: "Nabeel" });
-    // res.writeHead(404)
-
+app.get("*", (req, res) => {
+  res.render("error", { name: "Nabeel" });
+  // res.writeHead(404)
 });
 app.listen(port, () => {
-    console.log(`listening to the port of ${port}`);
+  console.log(`listening to the port of ${port}`);
 });
---
+++ public/js/main.js
@@ -0,0 +1,44 @@
+const cityName = document.getElementById("cityName");
+const submitButton = document.getElementById("submitButton");
+const city_name = document.getElementById("city-name");
+const temp_status = document.getElementById("temp-status");
+const temp = document.getElementById("temp");
+const dataHide = document.querySelector(".middle-layer");
+const getInfo = async (event) => {
+  event.preventDefault();
+  let cityVal = cityName.value;
+  if (cityVal == "") {
+    city_name.innerText = "PLease write te name before you search ";
+    dataHide.classList.add("data-hide");
+  } else {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=matric&callback=&appid=5064e39e4c4a34352a4691eda2db2627`;
+      const res = await fetch(url);
+      const objData = await res.json();
+      const arrayData = [objData];
+      console.log(url);
+
+      console.log(arrayData);
+      temp.innerText = (arrayData[0].main.temp-273).toFixed(2);
+      city_name.innerText = `${arrayData[0].name} ${arrayData[0].sys.country} `;
+      const tempMod = arrayData[0].weather[0].main;
+      if (tempMod == "Clear") {
+        temp_status.innerHTML =
+          '<i style="color: #fbfb98;" class="fa fa-sun"  aria-hidden="true"></i>';
+     } else if (tempMod == "Clouds") {
+        temp_status.innerHTML =
+          '<i style="color: #b4b4b4;" class="fa fa-cloud" aria-hidden="true"></i>';
+      } else if (tempMod == "Clouds") {
+        temp_status.innerHTML = '<i style="color: #5f91bd;" class="fas fa-cloud-rain"></i>';
+      } else {
+        temp_status.innerHTML =
+          '<i style="color: #fbfb98;"class="fa fa-sun" aria-hidden="true"></i>';
+      }
+      dataHide.classList.remove("data-hide");
+    } catch (error) {
+      city_name.innerText = "Please enter proper city name";
+      dataHide.classList.add("data-hide");
+    }
+  }
+};
+submitButton.addEventListener("click", getInfo);
